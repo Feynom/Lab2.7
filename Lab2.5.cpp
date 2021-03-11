3301d3f8fa18fde326371d22c7cc80b2891a2b96
Feynom <53947611+Feynom@users.noreply.github.com>
2021-03-11 13:06:42 +0200

    Add files via upload

---
+++ Lab2.5.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#pragma pack(1)
+
+#include "Fraction.h"
+
+int main()
+{
+    Pair a, b, c;
+    Fraction f;
+    int result;
+
+    cout << "\nPair a" << endl;
+    cin >> a;
+    cout << a;
+
+    cout << "\nPair b" << endl;
+    cin >> b;
+    cout << b;
+
+    cout << "\nPair c" << endl;
+    cin >> c;
+    cout << c;
+
+    cout << "\na and b" << endl;
+    result = f.ComparePairs(a, b);
+    f.ComparePairsResult(result);
+
+    cout << "\na and c" << endl;
+    result = f.ComparePairs(a, c);
+    f.ComparePairsResult(result);
+
+    cout << "\nb and c" << endl;
+    result = f.ComparePairs(b, c);
+    f.ComparePairsResult(result);
+
+    cout << "a++" << endl;
+    cout << a++ << endl;
+
+    cout << "a--" << endl;
+    cout << a-- << endl;
+
+    cout << "++a" << endl;
+    cout << ++a << endl;
+
+    cout << "--a" << endl;
+    cout << --a << endl;
+
+    cout << "Size of class is equal to " << sizeof(Fraction) << endl;
+
+    return 0;
+}
---
+++ UnitTest2.5.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../../Lab2.5/Lab2.5/Fraction.h"
+#include "../../Lab2.5/Lab2.5/Fraction.cpp"
+#include "../../Lab2.5/Lab2.5/Pair.h"
+#include "../../Lab2.5/Lab2.5/Pair.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest25
+{
+	TEST_CLASS(UnitTest25)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Pair a(1, 5), b(1, 5);
+			Assert::AreEqual(a == b, true);
+		}
+	};
+}
